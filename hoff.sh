daf180529890b7663c57599180d4e0381fba572b
Zachary <zjohnson@redventures.com>
2018-06-22 15:14:57 -0400

    osascript

--- hoff.sh
+++ hoff.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd ~/Downloads
-# curl -o thehoff.jpg http://ichef.bbci.co.uk/news/976/cpsprodpb/15F4F/production/_88553998_alamy_baywatch1_976.jpg
+cd $HOME/Downloads
+
 curl -o thehoff.jpg https://mjv46z.media.zestyio.com/David-Hasselhoff-flying-v2.f1cb27a519bdb5b6ed34049a5b86e317.jpg
 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/Downloads/thehoff.jpg'" && killall Dock
+p="$HOME/Downloads/thehoff.jpg"
+
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'$p'"'
+osascript -e 'tell application "Finder" to quit' -e 'delay 2' -e 'launch application "Finder"' 
