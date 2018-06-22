fbad942dbd8c36a46757fe612d4f523814b664b5
Zachary <zjohnson@redventures.com>
2018-06-22 12:40:40 -0400

    picture

--- hoff.sh
+++ hoff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Downloads
 # curl -o thehoff.jpg http://ichef.bbci.co.uk/news/976/cpsprodpb/15F4F/production/_88553998_alamy_baywatch1_976.jpg
-curl -o thehoff.jpg https://www.picsofcelebrities.com/celebrity/david-hasselhoff/pictures/large/david-hasselhoff-images.jpg
+curl -o thehoff.jpg https://mjv46z.media.zestyio.com/David-Hasselhoff-flying-v2.f1cb27a519bdb5b6ed34049a5b86e317.jpg
 
 sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/Downloads/thehoff.jpg'" && killall Dock
